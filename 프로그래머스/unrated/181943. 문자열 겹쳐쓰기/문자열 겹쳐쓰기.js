24981680c5418cbdd25f5cf158f9f4dee60b5996
Cheki <bom0704@naver.com>
2023-06-13 21:07:19 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,8 @@
 function solution(my_string, overwrite_string, s) {
     const str = [...my_string]
+//  문자열을 배열로 바꾸어주기
     str.splice(s,overwrite_string.length,overwrite_string)
+//  배열.splice(시작 index, 바꿀갯수, 추가할 요소)
     return str.join('')
+//  arr.join(요소구분 문자열)
 }
