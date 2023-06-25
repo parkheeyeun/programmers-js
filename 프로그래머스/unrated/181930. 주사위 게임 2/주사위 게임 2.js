357249bf75fe4c3af3df817571081f27f6437514
Cheki <bom0704@naver.com>
2023-06-25 19:54:13 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -8,3 +8,16 @@ function solution(a, b, c) {
         Math.pow(a,3)+ Math.pow(b,3)+ Math.pow(c,3))
     }
 }
+
+function solution(a, b, c) {
+    let sum1 = a + b + c;   // 모두 다른 경우
+    let sum2 = a * a + b * b + c * c;   // 2가지가 같은 경우
+    let sum3 = a ** 3 + b**3 + c**3;    // 모두 같은 경우
+
+    if (a === b && b === c)  // a===b이고 b===c이면 a===c는 증명할 필요 X
+        return sum1*sum2*sum3;
+     else if (a === b || a === c || b === c) 
+        return sum1*sum2;
+     else 
+        return sum1;
+}
