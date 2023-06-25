acd681d40cfa59b7d2680d7b78a41384c1b64e42
Cheki <bom0704@naver.com>
2023-06-25 21:30:07 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -4,12 +4,14 @@ function solution(num_list) {
     
     for(i=0; i<num_list.length; i++){
         if(num_list[i] % 2 === 1){
-            odd.push(String(num_list[i]))
+            odd.push(String(num_list[i])) 
+            // 숫자를 이어붙이기 위해 문자열로 바꾸고 push해서 odd라는 새로운 배열을 생성
         }else{
            even.push(String(num_list[i]))
         }
     }
-    odd_list = odd.join('')
+    
+    odd_list = odd.join('') // 이어붙이는 작업
     even_list = even.join('')
-    return Number(odd_list) + Number(even_list)
+    return Number(odd_list) + Number(even_list) // 다시 숫자로 바꾸어준다
 }
