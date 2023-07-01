379e5a9d1c57ce7e51e47fc4f1f2377036704a04
Cheki <bom0704@naver.com>
2023-07-01 18:27:24 +0900

    Update 배열 만들기 2.js

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -2,7 +2,11 @@ function solution(l, r) {
   const answer = [];
   
   for (let i = l; i <= r; i++) {
-    const num = i.toString();
+     const num = i.toString();
+      // 정규식은 슬래쉬로 감싸서 사용한다
+      // ^은 문자의 시작을 나타낸다
+      // $는 문자열의 끝을 나타낸다
+      // [] 안에 있는 것과 일치하는지 검사
     if (/^[05]+$/.test(num)) {
       answer.push(i);
     }
