ca52c1950bd338bb59e97d05417cdc68e12a7e4c
Cheki <bom0704@naver.com>
2023-07-12 20:17:52 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    let alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var answer = new Array(52).fill(0);
+    for (let i = 0; i < my_string.length; i++) {
+        answer[alp.indexOf(my_string[i])]++;
+    }
+    return answer;
+}
