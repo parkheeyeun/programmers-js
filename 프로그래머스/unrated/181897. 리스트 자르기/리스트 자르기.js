abcb4dde1caa13a9e30a3ecf57bacae77b247e7c
Cheki <bom0704@naver.com>
2023-07-13 16:49:29 +0900

    [unrated] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,24 @@
+function solution(n, slicer, num_list) {
+  const [a, b, c] = slicer;
+    let result
+
+  switch (n) {
+    case 1:
+      result = num_list.slice(0, b + 1);
+      break
+    case 2:
+      result =  num_list.slice(a);
+      break
+    case 3:
+      result =  num_list.slice(a, b + 1);
+      break
+    case 4:
+      let str = num_list.slice(a,b+1)
+      result = str.filter((_,i) => i%c===0)
+      break
+          
+    default:
+          break
+  }
+    return result
+}  
