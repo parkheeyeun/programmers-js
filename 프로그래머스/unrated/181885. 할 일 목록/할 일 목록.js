0377354da36d7a5938181c91454787e74373bea4
Cheki <bom0704@naver.com>
2023-07-17 16:35:15 +0900

    [unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,10 +1,8 @@
 function solution(todo_list, finished) {
     let not = []
-    let todo = [...todo_list]
     for(let i=0; i<todo_list.length; i++){
-       todo_list[i] = finished[i]
-        if(todo_list[i] === false){
-            not.push(todo[i])
+        if(finished[i] === false){
+            not.push(todo_list[i])
         }
     }return not
 }
