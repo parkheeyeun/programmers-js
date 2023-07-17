3dd6d23ebfe3dfc774a668b477920c8e1c0ffb92
Cheki <bom0704@naver.com>
2023-07-17 16:23:08 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,20 +1,13 @@
 function solution(num_list) {
-    let even = [] 
     let esum = 0
-    let odd = []
     let osum = 0
     for(let i=0; i<num_list.length; i++){
         if(i%2===0){
-           even.push(num_list[i])
+           esum += num_list[i]
         }else{
-            odd.push(num_list[i])
+           osum += num_list[i]
         }
     }
-    for(let j=0; j<even.length; j++){
-        esum += even[j]
-    }
-    for(let j=0; j<odd.length; j++){
-        osum += odd[j]
-    }
+    
     return osum > esum ? osum : esum
 }
