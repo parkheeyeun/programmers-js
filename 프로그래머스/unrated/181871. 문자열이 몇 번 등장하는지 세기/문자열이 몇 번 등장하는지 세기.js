182505aca14a9c9aecdde3236d8b081585fd5dda
Cheki <bom0704@naver.com>
2023-07-25 13:41:02 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,6 +1,6 @@
 function solution(myString, pat) {
     let count = 0
-   for(let i=0; i<myString.length-pat.length+1; i++){
+   for(let i=0; i<=myString.length-pat.length; i++){
        let str = myString.slice(i,i+pat.length)
        if(str === pat){
            count ++
