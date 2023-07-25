3321aba9d1c8941892b4392b59aaa30f3c287bf9
Cheki <bom0704@naver.com>
2023-07-25 13:59:55 +0900

    Update ad 제거하기.js

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.js
@@ -2,6 +2,7 @@ function solution(strArr) {
     for(let i=0; i<strArr.length; i++){
         if(strArr[i].includes("ad")===true){
              strArr.splice(i,1)
+             // 요소를 제거하면 배열의 길이가 하나 줄어들기 때문에 i--를 해준다
              i--
         }
     }return strArr
