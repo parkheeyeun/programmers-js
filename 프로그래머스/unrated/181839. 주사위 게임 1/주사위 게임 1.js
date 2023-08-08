edfb468353360144e054caf43fe631dd2d1af361
Cheki <bom0704@naver.com>
2023-08-08 18:50:41 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -6,4 +6,12 @@ function solution(a, b) {
     }else{
         return Math.abs(a-b)
     }
+    
+// 간단한 방법
+//     const isAOdd = a % 2 === 1
+//     const isBOdd = b % 2 === 1
+
+//     if(isAOdd && isBOdd) return Math.pow(a, 2) + Math.pow(b, 2)
+//     if(!isAOdd && !isBOdd) return Math.abs(a - b)
+//     return 2 * (a + b)
 }
